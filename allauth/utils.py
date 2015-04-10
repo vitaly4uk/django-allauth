6dbcb6aa06f1d09c2e6b791d9f4c4eae418ec67b
vitaly omelchuk <vitaly.omelchuk@gmail.com>
2015-04-10 16:57:19 +0300

    hack to not serialize django UUID fields

--- allauth/utils.py
+++ allauth/utils.py
@@ -164,7 +164,7 @@ def serialize_instance(instance):
     """
     ret = dict([(k, v)
                 for k, v in instance.__dict__.items()
-                if not k.startswith('_') or isinstance(v, UUID)])
+                if not k.startswith('_') and not isinstance(v, UUID)])
     return json.loads(json.dumps(ret, cls=DjangoJSONEncoder))
 
 
